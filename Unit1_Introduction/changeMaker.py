f3852b5176ab03b9368990f5b720d70a6f19ceec
rohanmalik <rohan_malik@ryecountryday.org>
2021-01-24 20:44:26 -0500

    First iteration of Change Maker

---
+++ Unit1_Introduction/changeMaker.py
@@ -0,0 +1,46 @@
+"""
+--- Change Maker ---
+Calculates how to most efficiently give change in bills and coins for a given transaction.
+Transaction amounts are randomly simulated between $100.00 and $10000.00
+"""
+
+import random
+import sys
+
+# Ask for the user's first and last name separately.
+first_name, last_name = input("Enter your first name: "), input("Enter your last name: ")
+
+# Print a greeting.
+print("Welcome to Embezzlement Bank Incorporated,", first_name, last_name)
+
+# TODO optimize the random amount generation
+amount_to_be_paid = int(100.00 + (random.random() * (10000.00 - 100.00)))
+print(f"Total to be paid: ${amount_to_be_paid/100}")
+
+# Ask for user's payment amount. This should be stored in a variable as dollars.
+amount_given = float(input("Amount paid: $"))
+amount_cents = 100*amount_given  # also storing the amount as cents for convenience
+
+# Calculate amount of change that should tendered.
+# subtract the amount paid from the amount needed; if not enough was paid, set to -1 as a flag for the next line.
+change = amount_cents - amount_to_be_paid if amount_cents >= amount_to_be_paid else -1
+if change == -1:  # if the person did not pay enough, tell them and close the program.
+    sys.exit("Pay up.")
+print(f"Change: ${change/100}")
+
+# Calculate amount of every denomination that should be given
+denominations = [2000, 1000, 500, 100, 25, 10, 5, 1]
+num_bills = []
+# loop through each denomination and append the maximum number of those that fits in the amount without going over
+# then update the amount to reflect the bills removed and move on to the next denomination.
+for denomination in denominations:
+    num_bills.append(change // denomination)
+    change %= denomination
+num_bills = list(map(lambda x: int(x), num_bills))  # convert each element, which is currently a float, to an integer
+
+# Print all information in a meaningful, readable manner.
+print("----------------")
+for i, denomination in enumerate(denominations):
+    print(f"${denomination/100}'s:".ljust(9), num_bills[i])  # left justify to make the amounts line up neatly
+print("----------------")
+print("Thank you for your business.")
