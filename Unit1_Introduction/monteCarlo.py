d2982b50ece0042759b537bd8a3953315a691a79
rohanmalik <rohan_malik@ryecountryday.org>
2021-01-25 21:53:56 -0500

    Added docstring and small improvements to pi calculator

--- Unit1_Introduction/monteCarlo.py
+++ Unit1_Introduction/monteCarlo.py
@@ -1,4 +1,9 @@
-# TODO add docstring and clean up comments
+"""
+--- Monte Carlo Simulation: Calculating π ---
+Uses a brute-force "Monte Carlo" method to calculate π by finding the ratio between the area of a circle of radius 1
+and that of a square with side length 2 by randomly placing points in the square and seeing how many land in the circle.
+This ratio will be π/4, so we can approximate π from there. Inefficient and inaccurate but simple method.
+"""
 
 import random
 
@@ -13,20 +18,14 @@ while True:
     # create a list of length num_points of random pairs of coordinates from -1 to 1, then prepare to print them
     pts = [(random.uniform(-1, 1), random.uniform(-1, 1)) for i in range(num_points)]
 
-    print("Generated coordinates:")
-    print("-----------------")
-
-    # TODO optimize algorithm?
     # which points are in the circle?
     num_in_circle = 0
-    for pt in pts:  # might as well print the points at the same time as you iterate through them to check if they are in the circle
-        print(pt)
+    for pt in pts:  # iterate through points to check
         if pt[0] ** 2 + pt[1] ** 2 <= 1:  # simplified distance formula if we consider the center as the origin of our coordinate system
             num_in_circle += 1
 
     # final calculations and outputs
     pi = 4 * num_in_circle / num_points
 
-    print("-----------------")
-    print("Points in circle:", num_in_circle)
+    print("Points in circle:", num_in_circle, "out of", num_points)
     print("Approximation: π =", pi)
