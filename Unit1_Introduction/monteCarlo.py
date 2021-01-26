e10bfde21b73e92f68a0bb327166b4eb2831117f
rohanmalik <rohan_malik@ryecountryday.org>
2021-01-26 10:09:50 -0500

    Improved and more concise distance checking algorithm

--- Unit1_Introduction/monteCarlo.py
+++ Unit1_Introduction/monteCarlo.py
@@ -2,7 +2,7 @@
 --- Monte Carlo Simulation: Calculating π ---
 Uses a brute-force "Monte Carlo" method to calculate π by finding the ratio between the area of a circle of radius 1
 and that of a square with side length 2 by randomly placing points in the square and seeing how many land in the circle.
-This ratio will be π/4, so we can approximate π from there. Inefficient and inaccurate but simple method.
+This ratio is π/4, so we can approximate π from there. Simple method, but inefficient and takes a while to become accurate.
 """
 
 import random
@@ -18,11 +18,10 @@ while True:
     # create a list of length num_points of random pairs of coordinates from -1 to 1, then prepare to print them
     pts = [(random.uniform(-1, 1), random.uniform(-1, 1)) for i in range(num_points)]
 
-    # which points are in the circle?
-    num_in_circle = 0
-    for pt in pts:  # iterate through points to check
-        if pt[0] ** 2 + pt[1] ** 2 <= 1:  # simplified distance formula if we consider the center as the origin of our coordinate system
-            num_in_circle += 1
+    # use list comprehension to select only the points that are within the circle (i.e. 1 unit or less away from origin)
+    # using a simplified distance formula to test all the points.
+    # Then simply take the length of that list to find the number of points in the circle
+    num_in_circle = len([point for point in pts if (point[0] ** 2) + (point[1] ** 2) <= 1])
 
     # final calculations and outputs
     pi = 4 * num_in_circle / num_points
