618e35931b1f705bcd5aea351f999edec0a94587
rohanmalik <rohan_malik@ryecountryday.org>
2021-01-28 13:22:28 -0500

    Add dictionary get operator test and prettify output

--- Unit2_BigO-Analysis/operationComplexity.py
+++ Unit2_BigO-Analysis/operationComplexity.py
@@ -7,13 +7,28 @@ def listIndex(alist, i):
     return alist[i]
 
 
+# test 2: dictionary get operator. implemented as O(1)
+def dictIndex(adict, i):
+    return adict[i]
+
+
 # test the operations for time complexity
+# TODO switch timing to timeit.Timer for higher accuracy and repeatability
 for listSize in range(1000, 10001, 1000):
-    nums = [randrange(0, 10000) for i in range(listSize)]  # list of random values from 0 to 10000
+    print("list size:", listSize, end=' || ')  # prepare for the later outputs
+
+    # create data structures for testing, made of random numbers from 1 to 10000
+    numList = [randrange(0, 10000) for i in range(listSize)]
+    numDict = {i: randrange(0, 10000) for i in range(listSize)}
     # determine the index separately instead of inline so the randrange() call doesn't interfere with timing
     rand_index = randrange(0, listSize - 1)
+    # test list get operator and print results
     start = time.time()
-    returned = listIndex(nums, rand_index)
+    listIndex(numList, rand_index)
     end = time.time()
-    # print summary of results
-    print(f"size: {listSize} returned: {returned} time: {end - start}")
+    print(f"list get: {round(end - start, 12)}s | ", end='')
+    # test dict get operator and print results
+    start = time.time()
+    listIndex(numDict, rand_index)
+    end = time.time()
+    print(f"dict get: {round(end - start, 12)}s")
