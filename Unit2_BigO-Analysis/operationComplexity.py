e10d675d44502ba91e83932d0010c720b561d015
rohanmalik <rohan_malik@ryecountryday.org>
2021-01-29 06:56:05 -0500

    Clean up code and change timing method

--- Unit2_BigO-Analysis/operationComplexity.py
+++ Unit2_BigO-Analysis/operationComplexity.py
@@ -1,8 +1,9 @@
 import time
+from timeit import Timer
 from random import randrange
 
 
-# test 1: list get operator. implemented as O(1), and results should reflect this
+# test 1: list get operator. implemented as O(1)
 def listGet(alist, i):
     return alist[i]
 
@@ -12,13 +13,13 @@ def dictGet(adict, i):
     return adict[i]
 
 
+# test 3: dictionary set operator. implemented as O(1)
 def dictSet(adict, i):
     adict[i] = 1234
     return adict
 
 
-# test the operations for time complexity
-# TODO switch timing to timeit.Timer for higher accuracy and repeatability
+# test the operations on increasingly large lists to see their time complexity
 for listSize in range(1000, 10001, 1000):
     print("list size:", listSize, end=' || ')  # prepare for the later outputs
 
@@ -26,19 +27,14 @@ for listSize in range(1000, 10001, 1000):
     numList = [randrange(0, 10000) for i in range(listSize)]
     numDict = {i: randrange(0, 10000) for i in range(listSize)}
     # determine the index separately instead of inline so the randrange() call doesn't interfere with timing
-    rand_index = randrange(0, listSize - 1)
-    # test list get operator and print results
-    start = time.time()
-    listGet(numList, rand_index)
-    end = time.time()
-    print(f"list get: {round(end - start, 12)}s | ", end='')
-    # do the same with dict get operator
-    start = time.time()
-    dictGet(numDict, rand_index)
-    end = time.time()
-    print(f"dict get: {round(end - start, 12)}s | ", end='')
-    # finally, test dict set operator
-    start = time.time()
-    dictSet(numDict, rand_index)
-    end = time.time()
-    print(f"dict set: {round(end - start, 12)}s")
+    randIndex = randrange(0, listSize - 1)
+
+    # create the timers that will run each of the tests
+    timer1 = Timer("listGet(numList, randIndex)", "from __main__ import listGet, numList, randIndex")
+    timer2 = Timer("dictGet(numDict, randIndex)", "from __main__ import dictGet, numDict, randIndex")
+    timer3 = Timer("dictSet(numDict, randIndex)", "from __main__ import dictSet, numDict, randIndex")
+
+    # test the three operations, each 10000 times to make up for the fact that they are so short
+    print(f"list get: {round(timer1.timeit(10000), 12)}s | ", end='')
+    print(f"dict get: {round(timer2.timeit(10000), 12)}s | ", end='')
+    print(f"dict set: {round(timer3.timeit(10000), 12)}s")
