3502a1b7306886994c557612a5395326d02a29b4
rohanmalik <rohan_malik@ryecountryday.org>
2021-01-30 21:59:27 -0500

    Clean up comments and add docstring for radix sort

--- Unit3_BasicDataStructures/radixSort.py
+++ Unit3_BasicDataStructures/radixSort.py
@@ -1,36 +1,34 @@
 import math
-import random
 
-listToSort = random.sample(range(1000), 10)
+"""
+--- Radix Sort ---
+Implementation of the Radix Sort algorithm for lists, which sorts numbers based on individual digits,
+going from lowest place value to highest and separating numbers into "bins" depending on the digit at that place
+and then combining the bins in order to incrementally bring the list closer to being sorted.
+Once every place value has been visited, the numbers will be completely sorted in ascending order.
+"""
 
 
 def radix(alist):
-    # take the log base 10 then floor it to get the nearest power of 10
-    # lower than the number, which is one less than the number of digits
-    # that the number has, because of how powers of 10 start from zero.
-    # and do this for the maximum number so we get the max number of digits
+    # take the log base 10 then floor it to get the nearest power of 10 lower than the number, which is
+    # one less than the number of digits that the number has, because of how powers of 10 start from zero.
+    # do this for the maximum number in the list so we get the max number of digits
     maxDigits = math.floor(math.log10(max(alist))) + 1
 
-    for i in range(maxDigits):  # as many passes as digits
-        # create our digit bins: a list of 10 lists, each representing one bin
+    for i in range(maxDigits):  # as many passes as there are digits
+        # create our digit bins: a list of 10 lists, each representing one bin.
+        # (note: the list passed in as a parameter is being used as the main bin.)
         digitBins = [[] for i in range(10)]
 
-        # this will get the desired digit of the number
-        # depending on which pass we are on
-        # by floor-dividing it by the corresponding power of 10
-        # which has the effect of truncating the number, such that
-        # the last digit is the one we want, then we can extract
-        # that digit using a modulo by 10
-        getDigit = lambda x: x // (10 ** i) % 10
-
-        # put the numbers in bins based on the relevant digit,
-        # as found by the lambda function above
-        # note: the list passed in as a parameter functions as the main bin
+        # now add each number to one of the digit bins based on the i-th digit from the right
+        # (we retrieve it by floor-dividing by 10^i so the last digit is the desired one, then taking mod 10)
+        # then we add our number to the bin corresponding to that digit
         for num in alist:
-            digitBins[getDigit(num)].append(num)
-        alist = []  # clear the main bin for refilling
-        # now refill the main bin
-        for bin in digitBins:
-            alist += bin  # the concatenation preserves order
+            digitBins[num // (10 ** i) % 10].append(num)
+
+        # clear the main bin, then refill it with the values from the individual bins in order
+        alist = []
+        for aBin in digitBins:
+            alist += aBin  # concatenation to the main list preserves order within each bin
 
     return alist
