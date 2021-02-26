e6ae87838a4cb261c7e30e4c9621a0ed931dedac
Rohan Malik <rohan_malik@ryecountryday.org>
2021-02-26 06:56:58 -0500

    Better feedback for validator function and modified user experience

--- Unit4_Recursion/_safeCrack.py
+++ Unit4_Recursion/_safeCrack.py
@@ -10,7 +10,7 @@ import random
 import os
 import time
 
-# -- validator --
+# -- VALIDATOR --
 
 # checks a user's password for validity based on the following conditions:
 # -> between 6 to 18 characters in length
@@ -23,7 +23,11 @@ import time
 def validate(password):
     # call our helper function with all the necessary starting parameter values. also do the initial length check
     # right now because we might as well, and exit out immediately if the password fails this
-    return validateHelper(password, 0, [0, 0, 0, 0], [" ", 0]) if 6 <= len(password) <= 20 else False
+    if 6 <= len(password) <= 20:
+        return validateHelper(password, 0, [0, 0, 0, 0], [" ", 0])
+    else:
+        print(f"Invalid: password is too {'long' if len(password) < 6 else 'short'} (must be 6-20 characters in length)")
+        return False
 
 
 # helper function that does all the work of validating the password; uses parameters to store information through recursion
@@ -35,16 +39,22 @@ def validate(password):
 #               consecutively. The variable starts at [" ", 0] and the counter is reset every time a new character is encountered
 #               but incremented if the currently stored character is encountered again.
 def validateHelper(password, i, reqs, inRow):
-    if i == len(password):
-        return reqs[0] >= 2 and reqs[1] >= 1 and reqs[2] >= 2 and reqs[3] >= 1
+    if i == len(password):  # this means we have reached the end of the password; now just check the requirements
+        if reqs[0] >= 2 and reqs[1] >= 1 and reqs[2] >= 2 and reqs[3] >= 1:
+            return True  # requirements are met and we have not exited out previously due to repeated chars --> we are done
+        else:
+            print("Invalid: missing requirements (need 2 uppercase, 1 lowercase, 2 digits, 1 special character)")
+            return False
 
-    if password[i] != inRow[0]:
+    if password[i] != inRow[0]:  # each time, check for repeated chars; if this is a new char, reset the repetition tracker
         inRow[0], inRow[1] = password[i], 1
-    else:
+    else:  # if a repeated char, increment the repetition tracker, but if it is already at 2, the password must be invalid
         if inRow[1] == 2:
+            print("Invalid: more than two of the same character in a row")
             return False
         inRow[1] += 1
 
+    # check for each of the requirements and increment the relevant indices of the requirement tracker
     if password[i].isupper():
         reqs[0] += 1
     elif password[i].islower():
@@ -54,47 +64,17 @@ def validateHelper(password, i, reqs, inRow):
     else:
         reqs[3] += 1
 
-    return validateHelper(password, i + 1, reqs, inRow)
+    return validateHelper(password, i + 1, reqs, inRow)  # call our function recursively, incrementing the index by 1
 
 
-# -- cracker --
+# -- CRACKER --
 
 # helper variable for brute forcing. indices -> uppercase: 0-25; lowercase: 26-51; numbers: 52-61; special chars: 62-84
 chars = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890!@#$%^&*()-+<>?:\"[]{}|."
 
-
-# uses brute force to attempt to guess the user's password, having only access to check an entire guess for equality
+# "brute force" cracker, but allows access to individual characters in the string
 # @param password: the target password to be guessed
-def bruteForce(password):
-    # have to test for each possible length of password from 6 to 15
-    for i in range(6, 16):
-        # call our main function with the password as a list for faster operations, the length as a parameter (for speed),
-        # our guess for the password (starts as all Qs, implemented as a list for faster get/set), the index we are
-        # currently at, which will keep track of recursion, and finally the number of tries (again as a list so it persists
-        # through recursion since lists are passed by reference but integers are not).
-        result = bruteForceHelper(list(password), i, ["Q"] * i, 0, [0])
-        if result is not None:  # if this length returned a match, we are done; if not, just try the next length
-            return result[0]
-
-
-# main function that does all the work; parameters discussed above
-def bruteForceHelper(password, length, guess, i, tries):
-    if i == length:  # if we have reached the end of the list in our recursion, check whether the guess was correct
-        tries[0] += 1  # since we are checking we have to add one try
-        return tries if guess == password else None
-    for char in chars:  # at each iteration, we go through every possible character that could be in the position
-        if guess[i-1] == char and guess[i-2] == char:  # if this would be the third character in a row, skip this branch
-            continue
-        guess[i] = char  # set the character of the guess to the current letter we are on
-        result = bruteForceHelper(password, length, guess, i + 1, tries)  # "branch" off into a new function call with this letter
-        if result is not None:  # if we received a match from this "branch", we are done, if not, try another branch
-            return result
-    return None  # if we did not receive a match from any letter in this branch, we made a mistake somewhere up the tree
-
-
-# new "brute force" cracker that allows access to individual characters in the string
-# @param password: the target password to be guessed
-# @param verbose: optional parameter, if set to true the brute force cracker will periodically output its guesses
+# @param verbose: optional parameter, if set to True the brute force cracker will periodically output its guesses
 def newBruteForce(password, verbose=False):
     # call our helper function with a few extra parameters: the list storing our guess (list for fast operations) and the
     # index we are currently at, which we need to keep track of our place through recursion. we additionally have to pass
@@ -137,11 +117,16 @@ def randomChars(numChars):
 
 # --- main program interface ---
 print("Welcome to SafeCrack")
-print("Are you looking to validate a password (v) or attempt to crack one (c)?")
-task = input("(Type 'v' or 'c'.) ")  # see what they want to do and slightly alter the prompt based on that
-password = getpass(prompt=f"Enter password to {'validate' if task == 'v' else 'crack'}: ")
-if task == "v":  # if they wanted to validate, perform that task and give feedback
-    print("Valid" if validate(password) else "Invalid")
-elif task == "c":  # if they wanted the cracker, clear the terminal to prepare and then run the cracker, providing feedback at the end
-    os.system("clear")
-    print(f"Guessed {password} in {newBruteForce(password, True)} tries")
+while True:  # main loop; has to be "while True" because the user can go on indefinitely
+    pwd = getpass(prompt="Enter a password: ")  # get the user's password securely
+    print("Validating...")
+    if validate(pwd):  # if the password is valid, give feedback and move on to cracking
+        print("Valid!")
+        input("Press enter to proceed to cracking...")  # wait for user feedback before proceeding
+        time.sleep(1)  # contributes to the realism a little bit
+        os.system("clear")  # prepare for the cracker
+        print(f"Guessed {pwd} in {newBruteForce(pwd, True)} tries")  # call the brute force method, with verbose mode on
+
+    if input("Press enter to run this utility again, or type any key to quit.") != "":  # no error handling needed because it is so broad
+        break  # if the user wanted to exit (anything but empty) then exit the loop
+print("Goodbye")
