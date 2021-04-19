a1a8ce45998b7ac506ade8b5c73c14701ab5a428
Rohan Malik <rohan_malik@ryecountryday.org>
2021-04-18 22:15:43 -0400

    Docstring and minor edits to map ADT

--- Unit5_SearchingSortingMaps/mapADT.py
+++ Unit5_SearchingSortingMaps/mapADT.py
@@ -1,31 +1,9 @@
-# class BaseMap:
-#
-#     class _MapEntry:
-#         def __init__(self, k, v):
-#             self._key = k
-#             self._value = v
-#
-#         def __eq__(self, other):
-#             return self._key == other.getKey()
-#
-#         def __ne__(self, other):
-#             return not self == other
-#
-#         def __lt__(self, other):
-#             return self._key < other.getKey()
-#
-#         def __gt__(self, other):
-#             return self._key > other.getKey()
-#
-#         def __str__(self):
-#             return "(" + str(self._key) + ", " + str(self._value) + ")"
-#
-#         def getKey(self):
-#             return self._key
-#
-#         def getVal(self):
-#             return self._value
-
+"""
+--- The Map ADT ---
+Implementation of a bare-bones Map ADT that includes all the standard operations such as get, set, delete, contains,
+length, string, and iterator methods, mostly overridden from Python defaults so it can interface more naturally with
+the language. Uses a helper class to encapsulate a map entry, that implements various comparison methods and string.
+"""
 
 class MapEntry:
     def __init__(self, k, v):
@@ -95,6 +73,7 @@ class UnsortedMap:
 
     def __iter__(self):
         return iter(self._map)
+
 
 # Testing
 testMap = UnsortedMap()
