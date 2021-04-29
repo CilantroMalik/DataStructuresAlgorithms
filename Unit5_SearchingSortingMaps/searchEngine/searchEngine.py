0a6ccf202e017d0eab3851f2fb9ebd1d2b8e31c9
Rohan Malik <rohan_malik@ryecountryday.org>
2021-04-29 10:42:02 -0400

    Continue working on SearchEngine

--- Unit5_SearchingSortingMaps/searchEngine/searchEngine.py
+++ Unit5_SearchingSortingMaps/searchEngine/searchEngine.py
@@ -18,4 +18,45 @@ with open("./News.txt", mode='r') as newsFile:
         articleDB[articleId] = articleText
         index = fullText.find("<ID>", index+1)
 
-print(articleDB)
+
+# -- helper classes and functions --
+class WordEntry:
+    def __init__(self, line, pos):
+        self.count = 1
+        self.locations = [(line, pos)]
+
+    def inc(self):
+        self.count += 1
+
+    def addLoc(self, line, pos):
+        self.locations.append((line, pos))
+
+
+def inc(aMap, key):
+    try:
+        aMap[key] += 1
+    except KeyError:
+        aMap[key] = 1
+
+
+def updateEntry(aMap, key, line, pos):
+    try:
+        aMap[key].addLoc(line, pos)
+        aMap[key].inc()
+    except KeyError:
+        aMap[key] = WordEntry(line, pos)
+
+
+# -- create a concordance for each article --
+# note: the full corpus concordance will just have word counts, but the individual article ones will store line
+#       number and position information (as this is not really applicable for the large scale concordance)
+fullCorpus = SortedMap()
+masterTable = []
+for article in articleDB:
+    localConcordance = SortedMap()
+    for i, line in enumerate(article.split("\n")):
+        for j, word in enumerate(line.split()):
+            if not word.lower().islower():
+                continue
+            inc(fullCorpus, word.lower())
+            updateEntry(localConcordance, word.lower(), i, j)
