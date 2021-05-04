f7acc12d50652e77ed80d8200b2efc2802bb26df
Rohan Malik <rohan_malik@ryecountryday.org>
2021-05-04 13:34:35 -0400

    Continue documenting SearchEngine algorithm

--- Unit5_SearchingSortingMaps/searchEngine/searchEngine.py
+++ Unit5_SearchingSortingMaps/searchEngine/searchEngine.py
@@ -103,53 +103,61 @@ if len(query.split(" ")) > 1:  # multi-word query handling
     # if no article had any occurrences of the phrase, this isn't a meaningful result; otherwise, this is the top result by default because it's an exact match
     if maxCountArticle == -1:
         topResult1 = articleDB[maxCount]  # store the article corresponding to the stored key we arrived at
-# single-word queries
-results = []
-for word in query.split(" "):
-    maxOccurrences, secondMaxOccurrences = 0, 0
-    maxArticle, secondMaxArticle = None, None
-    for article in articleDB:
-        concordance = masterTable[article.getKey()]
-        try:
-            if concordance[word].getCount() > maxOccurrences:
-                secondMaxOccurrences, secondMaxArticle = maxOccurrences, maxArticle
-                maxOccurrences, maxArticle = concordance[word].getCount(), article
-            elif concordance[word].getCount() > secondMaxOccurrences:
-                secondMaxOccurrences, secondMaxArticle = concordance[word].getCount(), article
-        except KeyError:
+# single-word analysis: using the concordances we created earlier
+results = []  # store all possibly relevant results before filtering them to the most relevant one or two
+for word in query.split(" "):  # search for each word separately
+    maxOccurrences, secondMaxOccurrences = 0, 0  # create variables for the highest and second highest count of the word
+    maxArticle, secondMaxArticle = None, None  # and corresponding ones for the articles that had those highest counts
+    for article in articleDB:  # go through every article and try to find the relevant entry in that article's local concordance
+        concordance = masterTable[article.getKey()]  # accessible from our map of maps with the article ID, due to the earlier setup
+        try:  # wrap in a try-except because the word may not be in the article at all
+            # we want to store the two largest counts of the word, which means we only pick out the articles whose counts are higher than the current stored values
+            if concordance[word].getCount() > maxOccurrences:  # use the stored count property of every entry in the concordance
+                secondMaxOccurrences, secondMaxArticle = maxOccurrences, maxArticle  # if larger than the max value, push the current max to second highest
+                maxOccurrences, maxArticle = concordance[word].getCount(), article  # and then replace the max with this one
+            elif concordance[word].getCount() > secondMaxOccurrences:  # if only larger than the second highest but not larger than the highest value
+                secondMaxOccurrences, secondMaxArticle = concordance[word].getCount(), article  # just replace the second highest with this article's count
+        except KeyError:  # if the word doesn't exist, we don't care, so the program just moves on without throwing an error and halting execution
             pass
-    if maxArticle is not None:
-        results.append((maxArticle, maxOccurrences))
-    if secondMaxArticle is not None:
+    if maxArticle is not None:  # if we found an article that has the word in it at least once, we add that article to the results
+        results.append((maxArticle, maxOccurrences))  # add the article key along with its number of occurrences, to be used as a weight later
+    if secondMaxArticle is not None:  # if we found a second highest article, add that as well and its number of occurrences
         results.append((secondMaxArticle, secondMaxOccurrences))
-articleWeights = SortedMap()
-for result in results:
+# result selection: use a system of weights to find the article with the highest "score" across all words in the query
+# this is to favor articles that placed highest or second highest for multiple words in the search query, if it was
+# more than one word; if it was a single word, it effectively behaves like one would expect and finds the simple maximum
+articleWeights = SortedMap()  # store weights in a map
+for result in results:  # go through every result and add the number of occurrences to that article's "score"
     try:
         articleWeights[result[0].getKey()] += result[1]
-    except KeyError:
+    except KeyError:  # if the key does not exist yet, we assign to it instead of incrementing it
         articleWeights[result[0].getKey()] = result[1]
-maxWeight, secondMaxWeight = 0, 0
+maxWeight, secondMaxWeight = 0, 0  # store the highest and second highest weights and their corresponding articles
 heaviest, secondHeaviest = None, None
-for item in articleWeights:
-    if item.getVal() > maxWeight:
-        secondMaxWeight = maxWeight
-        secondHeaviest = heaviest
-        maxWeight = item.getVal()
-        heaviest = item.getKey()
-    elif item.getVal() > secondMaxWeight:
-        secondMaxWeight = item.getVal()
-        secondHeaviest = item.getKey()
-if heaviest is None:
-    print("No results.")
-elif secondHeaviest is None:
-    print("Only one result...")
-    topResult1 = articleDB[heaviest]
-    print("----------------- First Result -----------------\n", topResult1)
-else:
-    if topResult1 is None:
+for item in articleWeights:  # for every weight, see if it is the highest or second highest
+    if item.getVal() > maxWeight:  # same logic as earlier: if larger than max, push max to second max and replace the max
+        secondMaxWeight, secondHeaviest = maxWeight, heaviest
+        maxWeight, heaviest = item.getVal(), item.getKey()  # the items here are article keys and their weights
+    elif item.getVal() > secondMaxWeight:  # and if larger than only the second max, just replace that
+        secondMaxWeight, secondHeaviest = item.getVal(), item.getKey()
+# final selection and displaying of results
+if heaviest is None:  # if there is no weight larger than 0, this variable will never be set, which means no word in the query exists in any article
+    print("No results.")  # so report as such
+elif secondHeaviest is None:  # if only one nonzero result was found, secondHeaviest will still be None, so we handle this case
+    if topResult1 is None:  # if there was no result from the multi-word phase, set the top result to our "heaviest" article
+        topResult1 = articleDB[heaviest]
+    else:  # if there was already a first result, set the second result to the heaviest, unless they are the same article in which case we have only one actual result
+        topResult2 = articleDB[heaviest] if articleDB[heaviest] != topResult1 else None
+    if topResult2 is None:  # if there was no second result, inform the user of this
+        print("Only one result...")
+    print("----------------- First Result -----------------\n", topResult1)  # we are guaranteed to have at least one result in this case
+    if topResult2 is not None:  # if we have two results, display both of them
+        print("----------------- Second Result -----------------\n", topResult2)
+else:  # finally, if both of the heaviest slots are occupied, we have potentially more than two top results
+    if topResult1 is None:  # if this was not already filled from the multi-word phase, populate both result slots with our highest weighted articles
         topResult1 = articleDB[heaviest]
         topResult2 = articleDB[secondHeaviest]
-    else:
+    else:  # if the first result slot is already full, put the heaviest article in the second (unless they are the same, in which case use the second heaviest)
         topResult2 = articleDB[heaviest] if articleDB[heaviest] != topResult1 else articleDB[secondHeaviest]
-    print("----------------- First Result -----------------\n", topResult1)
+    print("----------------- First Result -----------------\n", topResult1)  # finally, print both results
     print("----------------- Second Result -----------------\n", topResult2)
