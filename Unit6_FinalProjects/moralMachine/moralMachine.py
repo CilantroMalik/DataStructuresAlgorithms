ed903e0e62d86543b20d6e6c522d53bd33f2de17
Rohan Malik <rohan_malik@ryecountryday.org>
2021-05-22 08:28:43 -0400

    Add helper method and continue work on MoralMachine

--- Unit6_FinalProjects/moralMachine/moralMachine.py
+++ Unit6_FinalProjects/moralMachine/moralMachine.py
@@ -6,14 +6,39 @@ the face of seemingly objective, factual information such as laws and perceived 
 this algorithm represents one of many paths that a hypothetical AI could take to offering a solution to this problem.
 """
 
-# TODO write an enum for the possible types of occupants
+
+# Converts the given occupant string to an ID number that is easier to work with programmatically
+def mapToID(occupant):
+    if occupant == "Infant Child":
+        return 0
+    if occupant == "Small Child":
+        return 1
+    if occupant == "Teenage Child":
+        return 2
+    if occupant == "Adult":
+        return 3
+    if occupant == "Elderly Adult":
+        return 4
+    if occupant == "Small animal":
+        return 5
+    if occupant == "Large animal":
+        return 6
 
 
 # main function that encapsulates the algorithm that chooses the action the car will take
 def moralBrake(carOccupants: list, crosswalkOccupants: list, greenLight: bool):
-    driveStraight = True
-    # implement algorithm
+    driveStraight = True  # will store the final decision once we make it
+    # -- algorithm start --
+    # first, process the occupant lists
+    for i, entity in enumerate(carOccupants):
+        carOccupants[i] = mapToID(entity)
+    for i, entity in enumerate(crosswalkOccupants):
+        crosswalkOccupants[i] = mapToID(entity)
+    # then begin to work through cases one by one
+    # TODO: flesh this out
+
+    # finally, finish off the function
     return (
         "The vehicle will maintain its current course to drive through the intersection" if driveStraight
         else "The vehicle will change its course to drive into the barricade."
-    )
+    )  # convert the boolean to the corresponding string output for what action the car will take
