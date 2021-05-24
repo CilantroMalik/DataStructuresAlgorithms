504141008691a977a347c2bdca2dc038907c11fc
Rohan Malik <rohan_malik@ryecountryday.org>
2021-05-23 20:52:41 -0400

    Add weighting system and begin logic for MoralMachine

--- Unit6_FinalProjects/moralMachine/moralMachine.py
+++ Unit6_FinalProjects/moralMachine/moralMachine.py
@@ -7,7 +7,7 @@ this algorithm represents one of many paths that a hypothetical AI could take to
 """
 
 
-# Converts the given occupant string to an ID number that is easier to work with programmatically
+# converts the given occupant string to an ID number that is easier to work with programmatically
 def mapToID(occupant):
     if occupant == "Infant Child":
         return 0
@@ -25,17 +25,53 @@ def mapToID(occupant):
         return 6
 
 
+# returns a "weight" corresponding to each occupant ID that encapsulates its relative value when deciding whom to save
+def weight(occupant):
+    if occupant == 0:
+        return 7
+    if occupant == 1:
+        return 6
+    if occupant == 2:
+        return 5
+    if occupant == 3:
+        return 7
+    if occupant == 4:
+        return 3
+    if occupant == 5:
+        return 1
+    if occupant == 6:
+        return 2
+
+
 # main function that encapsulates the algorithm that chooses the action the car will take
 def moralBrake(carOccupants: list, crosswalkOccupants: list, greenLight: bool):
-    driveStraight = True  # will store the final decision once we make it
-    # -- algorithm start --
-    # first, process the occupant lists
+    # -- process the occupant lists --
     for i, entity in enumerate(carOccupants):
         carOccupants[i] = mapToID(entity)
     for i, entity in enumerate(crosswalkOccupants):
         crosswalkOccupants[i] = mapToID(entity)
-    # then begin to work through cases one by one
-    # TODO: flesh this out
+    # -- create helper variables and store useful quantities --
+    driveStraight = True  # keep track of the final decision once we make it; this value will be returned
+    # calculate and store the number of humans and animals in each group
+    numHumansCar, numHumansCrosswalk = len([0 for i in carOccupants if i <= 4]), len([0 for i in crosswalkOccupants if i <= 4])
+    numAnimalsCar, numAnimalsCrosswalk = len([0 for i in carOccupants if i >= 5]), len([0 for i in crosswalkOccupants if i >= 5])
+    # calculate the weights of each group of people
+    weightsCar, weightsCrosswalk = sum([weight(i) for i in carOccupants]), sum([weight(i) for i in crosswalkOccupants])
+    # -- algorithm start --
+    # we begin to work through cases one by one
+    # -> Case 1: pedestrians abiding by the law
+    if greenLight:
+        # Sub-case 1a: different numbers of humans and animals differ by at most 1 --> kill less people no matter what
+        if numHumansCar != numHumansCrosswalk and abs(numAnimalsCar - numAnimalsCrosswalk) <= 1:
+            driveStraight = numHumansCar > numHumansCrosswalk
+        # Sub-case 1b: different numbers of humans and animals differ by 2 or more --> use weighting system
+        # Sub-case 1c: same numbers of humans --> use weighting system
+        if abs(numAnimalsCar - numAnimalsCrosswalk) >= 2 or numHumansCar == numHumansCrosswalk:
+            driveStraight = weightsCar > weightsCrosswalk
+    # -> Case 2: pedestrians flouting the law
+    else:
+        # Sub-case 2a: same number of humans & animals
+        pass  # TODO finish this
 
     # finally, finish off the function
     return (
