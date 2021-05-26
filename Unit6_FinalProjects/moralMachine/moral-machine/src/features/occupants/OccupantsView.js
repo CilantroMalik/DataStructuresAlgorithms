ccf0a19a14f6455dada672a62a37a02b02854dd3
Rohan Malik <rohan_malik@ryecountryday.org>
2021-05-26 06:36:30 -0400

    Flesh out occupant display and CSS

--- Unit6_FinalProjects/moralMachine/moral-machine/src/features/occupants/OccupantsView.js
+++ Unit6_FinalProjects/moralMachine/moral-machine/src/features/occupants/OccupantsView.js
@@ -1,28 +1,28 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { nanoid } from '@reduxjs/toolkit'
+import { useSelector } from 'react-redux';
+import { nanoid } from '@reduxjs/toolkit';
 import styles from './OccupantsView.module.css';
 import '../../main.css';
 
 export const OccupantsView = () => {
-    const carOccupants = useSelector(state => state.car)
-    const crosswalkOccupants = useSelector(state => state.crosswalk)
+    const carOccupants = useSelector(state => state.occupants.car)
+    const crosswalkOccupants = useSelector(state => state.occupants.crosswalk)
 
     const carList = carOccupants.map(entity => (
-        <div className={styles.fadeIn}>
-            <h3>{entity}</h3>
+        <div key={nanoid()} className={styles.fadeIn}>
+            <h3 className={styles.occupantName} key={nanoid()}>{entity}</h3>
         </div>
     ))
     const crosswalkList = crosswalkOccupants.map(entity => (
-        <div className={styles.fadeIn}>
-            <h3 key={nanoid()}>{entity}</h3>
+        <div key={nanoid()} className={styles.fadeIn}>
+            <h3 className={styles.occupantName}>{entity}</h3>
         </div>
     ))
 
     return (
-        <div style={{display: "flex"}}>
-            <div>{carList}</div>
-            <div>{crosswalkList}</div>
+        <div className="flex-row" style={{justifyContent: "center", marginTop: "10px"}}>
+            <div className={styles.occupantList}>{carList}</div>
+            <div className={styles.occupantList}>{crosswalkList}</div>
         </div>
     )
 }
