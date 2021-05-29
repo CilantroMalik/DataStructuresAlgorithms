285c6dc02ebd5388bfcd08d23117d31f4ee5960b
Rohan Malik <rohan_malik@ryecountryday.org>
2021-05-29 11:22:08 -0400

    Finish documentation and do some cleanup

--- Unit6_FinalProjects/moralMachine/moral-machine/src/features/light/LightController.js
+++ Unit6_FinalProjects/moralMachine/moral-machine/src/features/light/LightController.js
@@ -4,14 +4,17 @@ import { toggleLight } from "./lightSlice";
 import '../../main.css';
 
 export const LightController = () => {
+    // need to fetch the current state of the light as well as be able to change it
     const light = useSelector(state => state.light.green)
     const dispatch = useDispatch()
 
+    // simply dispatch an action to toggle the light to the opposite of its current boolean state
     const onLightClicked = () => {
         dispatch(toggleLight(!light))
     }
 
+    // display a button that has different style amd text depending on the current light color
     return (
-        <button style={light ? {borderColor: "forestgreen", backgroundColor: "forestgreen", marginLeft: "27%"} : {borderColor: "indianred", backgroundColor: "indianred", marginLeft: "27%"}} onClick={onLightClicked} title="click to change light">{light ? "Green Light" : "Red Light"}</button>
+        <button style={{borderColor: (light ? "forestgreen" : "indianred"), backgroundColor: (light ? "forestgreen" : "indianred"), marginLeft: "27%"}} onClick={onLightClicked} title="click to change light">{light ? "Green Light" : "Red Light"}</button>
     )
 }
